86a31bc3bf089bea574d49d782acf05e0b29e5e0
Lehar <lehar@exdion.dev>
2025-11-15 16:57:23 +0530

    changed minipick to use fd and show hidden files

--- nvim/lua/plugins/mini_pick.lua
+++ nvim/lua/plugins/mini_pick.lua
@@ -1,32 +1,36 @@
 return {
-  'nvim-mini/mini.pick',
-  version = '*',
-  opts = {},
-  config = function(_, opts)
-    local pick = require('mini.pick')
-    pick.setup(opts)
+	"nvim-mini/mini.pick",
+	version = "*",
+	opts = {},
+	config = function(_, opts)
+		local pick = require("mini.pick")
+		pick.setup(opts)
 
-    -- vim.keymap.set('n', '<leader>ff', function() pick.builtin.files() end, { desc = 'Find files' })
-    vim.keymap.set('n', '<leader>ff', function()
-      require('mini.pick').start({
-        source = {
-          name = 'Files (All)',
-          cwd = vim.loop.cwd(),
-          items = function()
-          return vim.fn.systemlist(
-              'rg --files --hidden --follow ' ..
-              '--glob "!.venv/*" ' ..
-              '--glob "!.git/*" ' ..
-              '--glob "!node_modules/*"'
-            )
-          end,
-        },
-      })
-    end, { desc = 'Find files including hidden' })
-    vim.keymap.set('n', '<leader>fg', function() pick.builtin.grep_live() end, { desc = 'Live grep' })
-    vim.keymap.set('n', '<C-p>',       function() pick.builtin.git_files() end, { desc = 'Git files' })
-    vim.keymap.set('n', '<leader>fb', function() pick.builtin.buffers() end, { desc = 'Buffers' })
-    vim.keymap.set('n', '<leader>fh', function() pick.builtin.help() end, { desc = 'Help tags' })
-  end,
+		vim.keymap.set("n", "<leader>ff", function()
+			require("mini.pick").start({
+				source = {
+					name = "Files (fzf)",
+					items = function()
+						-- fd is better here, but fzf works too
+						return vim.fn.systemlist(
+							"fd --type f --hidden --follow --no-ignore " .. 
+                            "--exclude .git --exclude .venv --exclude node_modules --exclude __pycache__"
+						)
+					end,
+				},
+			})
+		end)
+		vim.keymap.set("n", "<leader>fg", function()
+			pick.builtin.grep_live()
+		end, { desc = "Live grep" })
+		vim.keymap.set("n", "<C-p>", function()
+			pick.builtin.git_files()
+		end, { desc = "Git files" })
+		vim.keymap.set("n", "<leader>fb", function()
+			pick.builtin.buffers()
+		end, { desc = "Buffers" })
+		vim.keymap.set("n", "<leader>fh", function()
+			pick.builtin.help()
+		end, { desc = "Help tags" })
+	end,
 }
-
